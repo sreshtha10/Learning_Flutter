406d4371e0dff135f97d0c2ea640b44f95768d43
Sreshtha <sreshtha.mehrotra@gmail.com>
2022-01-18 16:34:32 +0530

    Created simple ID card

--- lib/main.dart
+++ lib/main.dart
@@ -7,53 +7,96 @@ void main() {
 }
 
 class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        title: const Text('my first app'),
+        title: Text('ID Card'),
         centerTitle: true,
-        backgroundColor: Colors.red[600],
+        backgroundColor: Colors.grey[850],
+        elevation: 0.0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 40, 30,0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/asset1.jpg'),
+                radius: 40.0,
+              ),
+            ),
+            Divider(
+              height: 60.0,
+              color: Colors.grey[800],
+            ),
+            Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'Sreshtha Mehrotra',
+              style: TextStyle(
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 2,
+                fontWeight: FontWeight.bold,
+                fontSize: 28.0
 
-      ),
-      body: Row(
-        children: [
-          Expanded(
-            flex: 2,
-              child: Image.asset('assets/download.jpg')
-          ),
-          Expanded(
-            flex: 3,
-            child: Container(
-              padding: EdgeInsets.all(30.0),
-              color: Colors.cyan,
-              child: Text('1'),
+              ),
             ),
-          ),
-          Expanded(
-            flex: 2,
-            child: Container(
-              padding: EdgeInsets.all(30.0),
-              color: Colors.pinkAccent,
-              child: Text('2'),
+            SizedBox(
+              height: 30,
             ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
-              padding: EdgeInsets.all(30.0),
-              color: Colors.amber,
-              child: Text('3'),
+            Text(
+              'CURRENT NINJA LEVEL',
+              style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2
+              ),
             ),
-          )
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: const Text('Click'),
-        onPressed: null,
-        backgroundColor: Colors.red[600],
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              '8',
+              style: TextStyle(
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 2,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 28.0
+
+              ),
+            ),
+            SizedBox(height: 30,),
+            Row(
+              children: [
+                Icon(
+                  Icons.email_outlined,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(width: 10.0),
+                Text(
+                  'sreshtha.mehrotra@gmail.com',
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 18.0,
+                    letterSpacing: 1
+                  ),
+                )
+              ],
+            )
+
+          ],
+        ),
       ),
     );
   }
